0523e6f368b8dc59554a597f185057609b9d5419
Dongwook3 <d_shin2@fanshaweonline.ca>
2017-01-29 18:45:37 -0500

    Add files via upload
    sorry for sending project file too late.
    we have to add bio information each other. Please finish.

---
+++ js/changingThings.js
@@ -0,0 +1,13 @@
+// change elements with classList
+(function(){
+	var theImages = document.querySelectorAll('.portfolioCon'),
+		theHeading = document.querySelector('.heading'),
+		theSubhead = document.querySelector('.mainDesk h2'),
+		theSeasonText = document.querySelector('.mainDesk p'),
+		appliedClass; // set our globals
+	
+	function changeElements() {
+		// change content on the page
+	}
+
+})();
